c6a2dfa2715a91aeb337d5a18a2afe4a7150f998
Andrei <ovidiu.andrey97@gmail.com>
2018-11-12 18:17:12 +0000

    added a webhook submission page, not final

--- index.py
+++ index.py
@@ -1,8 +1,15 @@
 from flask import Flask, redirect , url_for, render_template, abort, request
+import bcrypt
+import sqlite3
 app = Flask(__name__)
+
+
 @app.route('/')
 def root():
     return render_template('index.html')
+@app.route('/webhook/')
+def webh():
+    return render_template('webhook.html')
 
 if __name__=="__main__":
     app.run(host='localhost', debug=True)
