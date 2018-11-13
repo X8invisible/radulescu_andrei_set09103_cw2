141685fa4263aca7cda53db245ba288248f28d70
Ovidiu-Andrei Radulescu <ovidiu.andrey97@gmail.com>
2018-11-13 14:22:48 +0000

    added profile page, made sessions

--- index.py
+++ index.py
@@ -1,13 +1,35 @@
-from flask import Flask, redirect , url_for, render_template, abort, request
+from flask import Flask, redirect , url_for, render_template, abort, request, session, flash
 import bcrypt
 import sqlite3
+import ConfigParser
 import dataManager as dbManager
 app = Flask(__name__)
 
+def init(app):
+    config = ConfigParser.ConfigParser()
+    try:
+        config_location = "etc/defaults.cfg"
+        config.read(config_location)
+
+        app.config['DEBUG'] = config.get("config", "debug")
+        app.config['ip_address'] = config.get("config", "ip_address")
+        app.config['port'] = config.get("config", "port")
+        app.config['url'] = config.get("config", "url")
+        app.config['SECRET_KEY'] = config.get("config", "SECRET_KEY")
+    except:
+        print "Could not read config from: ", config_location
 
 @app.route('/')
 def root():
-    return render_template('index.html')
+    try:
+        if(session['name']):
+            logged = True
+            name = session['name']
+    except KeyError:
+            logged = False
+            name = "Account"
+            pass
+    return render_template('index.html', logged = logged, name = name, title = "Home")
 @app.route('/webhook/')
 def webh():
     return render_template('webhook.html')
@@ -19,20 +41,41 @@ def register():
         dbManager.signUp(username, password)
         return redirect('/')
     else:
-        return render_template('signup.html')
+        return render_template('signup.html', logged = False, name = "Account", title = "Sign Up")
 @app.route('/login', methods=['POST', 'GET'])
 def log():
     if request.method=='POST':
         username = request.form['username']
         password = request.form['password']
         if dbManager.checkLogIn(username, password):
-            return('good boi')
+            session['name'] = username
+            return redirect('/')
         else:
             return('bad boi')
     else:
-        return render_template('signup.html')
+        try:
+            if(session['name']):
+                logged = True
+                name = session['name']
+                flash('You are already Logged In! ')
+        except KeyError:
+                logged = False
+                name = "Account"
+                pass
+        return render_template('signup.html', logged = logged, name = name, title = "Log In")
+@app.route('/profile/')
+def dashboard():
+    return render_template('profile.html', logged = False, name = "Account", title = "Dashboard")
 @app.route('/logout')
 def logout():
+    session.pop('name', None)
     return redirect('/')
+
+app.secret_key = app.config['SECRET_KEY']
 if __name__=="__main__":
-    app.run(host='localhost', debug=True)
+    init(app)
+    app.run(
+        host = app.config['ip_address'],
+        port = int(app.config['port']),
+        debug = app.config['DEBUG']
+    )
